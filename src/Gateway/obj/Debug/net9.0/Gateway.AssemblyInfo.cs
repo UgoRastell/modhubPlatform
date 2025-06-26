c7cead85c5356743c3333b262c23b4ebfa708e8f
UgoRastell <urastell@normandiewebschool.fr>
2025-06-26 16:24:29 +0200

    remove appbar

--- src/Gateway/obj/Debug/net9.0/Gateway.AssemblyInfo.cs
+++ src/Gateway/obj/Debug/net9.0/Gateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07291e89e0936cdb3af8daa64d35bc61225a9f25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4406c66f14115f12099ef07680d990e0e058d5c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Shared/obj/Debug/net9.0/Shared.AssemblyInfo.cs
+++ src/Shared/obj/Debug/net9.0/Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07291e89e0936cdb3af8daa64d35bc61225a9f25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4406c66f14115f12099ef07680d990e0e058d5c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
