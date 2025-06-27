a9323049f41dba4baa96a0bb47abe92fb1f7a927
UgoRastell <urastell@normandiewebschool.fr>
2025-06-27 09:06:58 +0200

    page game.rzor fix error 404

--- src/Shared/obj/Debug/net9.0/Shared.AssemblyInfo.cs
+++ src/Shared/obj/Debug/net9.0/Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7cead85c5356743c3333b262c23b4ebfa708e8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dd71ab70d22ecd74a1e477716e2073207818b44")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
