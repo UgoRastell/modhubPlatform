bca7d5b9d5307ad85e3e76ad0c6d1985a03e02d3
UgoRastell <urastell@normandiewebschool.fr>
2025-07-03 15:57:02 +0200

    test

--- src/Shared/obj/Debug/net9.0/Shared.AssemblyInfo.cs
+++ src/Shared/obj/Debug/net9.0/Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79b1f75f0895840b10bbaea65902bdb8ef8271e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c6fa4dc8a67be97d614eb6744f08712b2187647")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
