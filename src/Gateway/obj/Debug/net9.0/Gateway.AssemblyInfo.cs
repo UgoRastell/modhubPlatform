79b1f75f0895840b10bbaea65902bdb8ef8271e0
UgoRastell <urastell@normandiewebschool.fr>
2025-07-03 11:04:05 +0200

    test

--- src/Gateway/obj/Debug/net9.0/Gateway.AssemblyInfo.cs
+++ src/Gateway/obj/Debug/net9.0/Gateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fee455c9afc3701047f21db3f9bfbbf56ae489bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6605921b4618994ca0e5515d78d46dfdb00c0865")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Services/Community/obj/Debug/net9.0/CommunityService.AssemblyInfo.cs
+++ src/Services/Community/obj/Debug/net9.0/CommunityService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommunityService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fee455c9afc3701047f21db3f9bfbbf56ae489bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6605921b4618994ca0e5515d78d46dfdb00c0865")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommunityService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommunityService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
