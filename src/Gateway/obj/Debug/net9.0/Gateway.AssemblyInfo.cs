078b05edfec39813eb598944b6dd3e7966227e9f
UgoRastell <urastell@normandiewebschool.fr>
2025-07-03 16:13:35 +0200

    test

--- src/Gateway/obj/Debug/net9.0/Gateway.AssemblyInfo.cs
+++ src/Gateway/obj/Debug/net9.0/Gateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c6fa4dc8a67be97d614eb6744f08712b2187647")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bca7d5b9d5307ad85e3e76ad0c6d1985a03e02d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
