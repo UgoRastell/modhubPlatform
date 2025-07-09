24f4622e44b432da9c739e2c985f2ffdccfaa88e
Ugo RASTELL <ugo.rastell@outlook.fr>
2025-07-09 13:33:12 +0200

    test

--- src/Frontend/obj/Debug/net9.0/Frontend.AssemblyInfo.cs
+++ src/Frontend/obj/Debug/net9.0/Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a78d93011856d384079a2c2c68cb4031b883c983")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0542918f2b0be18c43c84c10f845c31ef82b9518")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Services/Community/obj/Debug/net9.0/CommunityService.AssemblyInfo.cs
+++ src/Services/Community/obj/Debug/net9.0/CommunityService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommunityService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a78d93011856d384079a2c2c68cb4031b883c983")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0542918f2b0be18c43c84c10f845c31ef82b9518")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommunityService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommunityService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
