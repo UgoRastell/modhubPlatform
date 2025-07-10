4f7a9529817cf04aae2dac851e1ad7856f19fff0
UgoRastell <urastell@normandiewebschool.fr>
2025-07-10 15:49:07 +0200

    test

--- src/Frontend/obj/Debug/net9.0/Frontend.AssemblyInfo.cs
+++ src/Frontend/obj/Debug/net9.0/Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f94372747841840ccc68fbec756a152a58a90541")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94b01919ab093d1bcbc0f93f8249778e356c2e19")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
